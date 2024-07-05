5b4f7a387a651355096d4ef286483e15187f34b7
rahnoff <gbennett281@gmail.com>
2024-07-05 16:25:50 -0500

    Add API

--- src/Ordering.API/Program.cs
+++ src/Ordering.API/Program.cs
@@ -1,33 +1,21 @@
+﻿var builder = WebApplication.CreateBuilder(args);
 
-namespace Ordering.API;
+builder.AddServiceDefaults();
+builder.AddApplicationServices();
+builder.Services.AddProblemDetails();
 
-public class Program
-{
-    public static void Main(string[] args)
-    {
-        var builder = WebApplication.CreateBuilder(args);
+var withApiVersioning = builder.Services.AddApiVersioning();
 
-        // Add services to the container.
+builder.AddDefaultOpenApi(withApiVersioning);
 
-        builder.Services.AddControllers();
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
+var app = builder.Build();
 
-        var app = builder.Build();
+app.MapDefaultEndpoints();
 
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
+var orders = app.NewVersionedApi("Orders");
 
-        app.UseAuthorization();
+orders.MapOrdersApiV1()
+      .RequireAuthorization();
 
-
-        app.MapControllers();
-
-        app.Run();
-    }
-}
+app.UseDefaultOpenApi();
+app.Run();
